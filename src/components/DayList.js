fd1eaafed455841d32b8c3705c948ee14e1bcb77
amyleblanc <amymccarthy2014@gmail.com>
2022-02-24 18:12:34 -0800

    create component; map over days array

---
+++ src/components/DayList.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import DayListItem from "components/DayListItem";
+
+export default function DayList({days, day, setDay}) {
+  return (
+    <>
+      <ul>
+        {
+          days.map(d => (
+            <DayListItem 
+              key={d.id} 
+              name={d.name} 
+              spots={d.spots} 
+              selected={d.name === day} 
+              setDay={setDay}
+            />
+          ))
+        }
+      </ul>
+    </>
+  );
+}
