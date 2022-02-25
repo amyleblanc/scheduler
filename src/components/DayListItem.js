613c8c147bb989b5439fba9a9d80c85ae05ffe1c
amyleblanc <amymccarthy2014@gmail.com>
2022-02-24 16:14:27 -0800

    add classNames & function to call spots remaining

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,30 @@
 import React from "react";
 
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
 export default function DayListItem(props) {
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": (props.spots === 0)
+  });
+
+  function formatSpots() {
+      if (!props.spots) {
+        return "no spots remaining";
+      }
+
+      if (props.spots === 1) {
+        return "1 spot remaining";
+      }
+
+      return `${props.spots} spots remaining`;
+  }
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
