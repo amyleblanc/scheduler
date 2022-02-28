2f284d221835d1c48270cf5ef73fbd9efbe39829
amyleblanc <amymccarthy2014@gmail.com>
2022-02-27 20:50:13 -0800

    add function to display text if 'time' prop exists

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,8 +3,16 @@ import React from "react";
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
+  function showAppointments() {
+    if (!props.time) {
+      return "No Appointments";
+    }
+    if (props.time) {
+      return `Appointment at ${props.time}`;
+    }
+  }
 
   return (
-    <article className="appointment"></article>
+    <article className="appointment">{showAppointments()}</article>
   )
 }
