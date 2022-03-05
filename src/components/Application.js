119456294627222fd12ae9d1c6677f265875f514
amyleblanc <amymccarthy2014@gmail.com>
2022-03-05 14:50:45 -0800

    replace appointments object with dailyAppointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,45 +5,6 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
-
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
@@ -52,6 +13,8 @@ export default function Application(props) {
   });
   const setDay = (day) => setState({ ...state, day });
   const setDays = (days) => setState(prev => ({ ...prev, days }));
+
+  const dailyAppointments = [];
 
   useEffect(() => {
     axios
@@ -89,7 +52,7 @@ export default function Application(props) {
       <section className="schedule">
         <ul>
           {
-            Object.values(appointments).map(appointment => (
+            dailyAppointments.map(appointment => (
               <Appointment 
                 key={appointment.id}
                 {...appointment}
