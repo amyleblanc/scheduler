cbb1cf70beb1bf7706644126423f29b9ce802164
amyleblanc <amymccarthy2014@gmail.com>
2022-03-05 14:38:06 -0800

    update DayList props for day & days state

--- src/components/Application.js
+++ src/components/Application.js
@@ -50,7 +50,7 @@ export default function Application(props) {
     days: [],
     appointments: {}
   });
-
+  
   useEffect(() => {
     axios
       .get("/api/days")
@@ -73,8 +73,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
