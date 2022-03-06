3642e6a920f16abb4f7ef26bfc2388474fd901c5
amyleblanc <amymccarthy2014@gmail.com>
2022-03-05 20:14:25 -0800

    use getAppointmentsForDay() for Appointment data

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
+import { getAppointmentsForDay } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -13,23 +14,23 @@ export default function Application(props) {
   });
   const setDay = (day) => setState({ ...state, day });
 
-  const dailyAppointments = [];
-
   useEffect(() => {
     Promise.all([
       axios.get("/api/days"), 
       axios.get("/api/appointments")
     ]).then((all) => {
-        setState((prev) => ({
-          ...prev,
-          days: all[0].data,
-          appointments: all[1].data,
-        }));
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+      setState((prev) => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+      }));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
   }, []);
+
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   return (
     <main className="layout">
