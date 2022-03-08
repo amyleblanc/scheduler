84971860cada782b9fcaf24b129ed02247c2b2c3
amyleblanc <amymccarthy2014@gmail.com>
2022-03-07 18:22:25 -0800

    import useVisualMode; render SHOW/EMPTY using mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,13 @@
 import React, { Fragment } from "react";
+import useVisualMode from "hooks/useVisualMode";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 
 import "components/Appointment/styles.scss";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
 
 export default function Appointment(props) {
   function showAppointments() {
@@ -15,11 +19,16 @@ export default function Appointment(props) {
     }
   }
 
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <>
       <article className="appointment">
         <Header time={showAppointments()} />
-        {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer.name} /> : <Empty />}
+        {mode === EMPTY && <Empty />}
+        {mode === SHOW && <Show student={props.interview.student} interviewer={props.interview.interviewer.name} />}
       </article>
     </>
   )
