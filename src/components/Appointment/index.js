8f3144d95817233ab81d7fc21868d31b90c04d8f
amyleblanc <amymccarthy2014@gmail.com>
2022-03-07 19:00:50 -0800

    bugfix: remove student & interviewer from Form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -31,7 +31,7 @@ export default function Appointment(props) {
         <Header time={showAppointments()} />
         {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
         {mode === SHOW && <Show student={props.interview.student} interviewer={props.interview.interviewer.name} />}
-        {mode === CREATE && <Form id={props.id} time={props.time} student={props.interview.student} interviewer={props.interview.interviewer.name} interviewers={[]} />}
+        {mode === CREATE && <Form id={props.id} time={props.time} interviewers={[]} />}
       </article>
     </>
   )
