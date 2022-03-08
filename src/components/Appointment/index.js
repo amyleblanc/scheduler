70a7d69e53813ec76d994f6c9dad6272a4683224
amyleblanc <amymccarthy2014@gmail.com>
2022-03-07 18:55:15 -0800

    add 'CREATE' mode transition to Form with props

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,6 +3,7 @@ import useVisualMode from "hooks/useVisualMode";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import Form from "components/Appointment/Form";
 
 import "components/Appointment/styles.scss";
 
@@ -30,6 +31,7 @@ export default function Appointment(props) {
         <Header time={showAppointments()} />
         {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
         {mode === SHOW && <Show student={props.interview.student} interviewer={props.interview.interviewer.name} />}
+        {mode === CREATE && <Form id={props.id} time={props.time} student={props.interview.student} interviewer={props.interview.interviewer.name} interviewers={[]} />}
       </article>
     </>
   )
