ed45a0936799ff1bc4bcf6af2339b253ce610fbc
amyleblanc <amymccarthy2014@gmail.com>
2022-03-09 17:04:38 -0800

    add save() to bookInterview & transition to SHOW

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -25,13 +25,22 @@ export default function Appointment(props) {
     props.interview ? SHOW : EMPTY
   );
 
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    props.bookInterview(props.id, interview)
+    transition(SHOW);
+  }
+
   return (
     <>
       <article className="appointment">
         <Header time={showAppointments()} />
         {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
         {mode === SHOW && <Show student={props.interview.student} interviewer={props.interview.interviewer.name} />}
-        {mode === CREATE && <Form id={props.id} time={props.time} interviewers={props.interviewers} onCancel={back} />}
+        {mode === CREATE && <Form id={props.id} time={props.time} interviewers={props.interviewers} onCancel={back} onSave={save} />}
       </article>
     </>
   )
