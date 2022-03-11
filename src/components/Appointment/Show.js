d30aee8559249c6ad4228a0c51dd019ee85f5811
amyleblanc <amymccarthy2014@gmail.com>
2022-03-10 16:13:18 -0800

    create function to call props.onDelete(props.id)

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -2,6 +2,10 @@ import React from "react";
 
 export default function Show(props) {
 
+  const confirmDelete = () => {
+    return props.onDelete(props.id);
+  }
+
   return (
     <main className="appointment__card appointment__card--show">
       <section className="appointment__card-left">
@@ -23,7 +27,7 @@ export default function Show(props) {
             className="appointment__actions-button"
             src="images/trash.png"
             alt="Delete"
-            onClick={props.onDelete}
+            onClick={confirmDelete}
           />
         </section>
       </section>
