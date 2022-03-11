376d3639f4807b868d639d4f00f898dda5ed966d
amyleblanc <amymccarthy2014@gmail.com>
2022-03-10 16:14:10 -0800

    create function to call props.onConfirm(props.id)

--- src/components/Appointment/Confirm.js
+++ src/components/Appointment/Confirm.js
@@ -2,13 +2,16 @@ import React from "react";
 import Button from "components/Button";
 
 export default function Confirm(props) {
-
+  const confirm = () => {
+    return props.onConfirm(props.id);
+  }
+console.log("confirm: ", props);
   return (
     <main className="appointment__card appointment__card--confirm">
       <h1 className="text--semi-bold">{props.message}</h1>
       <section className="appointment__actions">
         <Button onClick={props.onCancel} danger>Cancel</Button>
-        <Button onClick={props.onConfirm} danger>Confirm</Button>
+        <Button onClick={confirm} danger>Confirm</Button>
       </section>
     </main>
   );
