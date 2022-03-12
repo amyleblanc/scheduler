691ba3864b26a8a348eacb4b933e2b5e9d8e81f6
amyleblanc <amymccarthy2014@gmail.com>
2022-03-12 14:37:39 -0800

    create Hook to provide state & change state

---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+
+export default function useApplicationData() {
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+  const setDay = (day) => setState({ ...state, day });
+  
+  useEffect(() => {
+    Promise.all([
+      axios.get("/api/days"), 
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then((all) => {
+      setState((prev) => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data
+      }));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }, []);
+  
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview },
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
+    };
+
+    return axios
+      .put(`/api/appointments/${id}`, appointment)
+      .then(() => {
+        setState({
+          ...state,
+          appointments
+        });
+      })
+  }
+
+  function deleteInterview(id) {
+    const nullInterview = {
+      ...state.appointments[id],
+      interview: null
+    };
+    const updatedAppointments = {
+      ...state.appointments,
+      [id]: nullInterview
+    };
+
+    return axios
+      .delete(`/api/appointments/${id}`, nullInterview)
+      .then(() => {
+        setState({
+          ...state,
+          updatedAppointments
+        });
+      })
+  }
+
+  return {
+    state,
+    setDay,
+    bookInterview,
+    deleteInterview
+  };
+};
