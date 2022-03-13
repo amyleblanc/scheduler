62d209d08651749073cb1899cc77330de75ca188
amyleblanc <amymccarthy2014@gmail.com>
2022-03-12 23:01:36 -0800

    code formatting fixes

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -31,20 +31,21 @@ export default function useApplicationData() {
   
   useEffect(() => {
     Promise.all([
-      axios.get("/api/days"), 
+      axios.get("/api/days"),
       axios.get("/api/appointments"),
       axios.get("/api/interviewers")
-    ]).then((all) => {
-      setState((prev) => ({
-        ...prev,
-        days: all[0].data,
-        appointments: all[1].data,
-        interviewers: all[2].data
-      }));
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+    ])
+      .then((all) => {
+        setState((prev) => ({
+          ...prev,
+          days: all[0].data,
+          appointments: all[1].data,
+          interviewers: all[2].data
+        }));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
   
   function bookInterview(id, interview) {
@@ -66,7 +67,7 @@ export default function useApplicationData() {
           appointments,
           days: updatedDays
         }));
-      })
+      });
   }
 
   function deleteInterview(id) {
@@ -88,7 +89,7 @@ export default function useApplicationData() {
           appointments: updatedAppointments,
           days: updatedDays
         }));
-      })
+      });
   }
 
   return {
