62ecada38efb721c8de50358be8037ab0eda0847
amyleblanc <amymccarthy2014@gmail.com>
2022-03-20 21:18:17 -0700

    test bugfix: remove interviewer validation

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -24,10 +24,6 @@ export default function Form(props) {
       setError("Student name cannot be blank");
       return;
     }
-    if (interviewer === null) {
-      setError("Please select an interviewer");
-      return;
-    }
     props.onSave(student, interviewer);
   }
 
