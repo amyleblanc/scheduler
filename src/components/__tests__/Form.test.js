66e3a5e93977731b720ee2abdcc3076a9056a843
amyleblanc <amymccarthy2014@gmail.com>
2022-03-20 21:36:43 -0700

    test for user interaction/input field value change

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -41,5 +41,20 @@ describe("Form tests: ", () => {
     expect(handleClick).toHaveBeenCalledTimes(1);
     expect(handleClick).toHaveBeenCalledWith("Lydia Miller-Jones", null);
   });
+
+  it("submits the name entered by the user", () => {
+    const onSave = jest.fn();
+    const { getByText, getByPlaceholderText } = render(
+      <Form interviewers={interviewers} onSave={onSave} />
+    );
+  
+    const input = getByPlaceholderText("Enter Student Name");
+  
+    fireEvent.change(input, { target: { value: "Lydia Miller-Jones" } });
+    fireEvent.click(getByText("Save"));
+  
+    expect(onSave).toHaveBeenCalledTimes(1);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
+  });
 });
 
