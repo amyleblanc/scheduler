8874553ca1fe943a457c9f85c0316f878dd287e9
amyleblanc <amymccarthy2014@gmail.com>
2022-03-21 19:52:49 -0700

    access first appointment element in container

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -17,7 +17,9 @@ describe("Application", () => {
 
   it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
     const { container } = render(<Application />);
+    
     await waitForElement(() => getByText(container, "Archie Cohen"));
-    console.log(prettyDOM(container));
+    const appointment = getAllByTestId(container, "appointment")[0];
+    console.log(prettyDOM(appointment));
   });
 });
