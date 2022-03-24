bc6ff697bf2cb83ac523ee6744527c4016ea5f67
amyleblanc <amymccarthy2014@gmail.com>
2022-03-23 19:58:49 -0700

    edit PUT request to accept both appointment routes

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -81,7 +81,7 @@ export default {
   }),
 
   put: jest.fn(url => {
-    if (url === "/api/appointments/1") {
+    if (url === "/api/appointments/1" || url === "/api/appointments/2") {
       return Promise.resolve({
         status: 200,
         statusText: "No Content"
