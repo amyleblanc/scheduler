22aa9f7ddcbf1714e7650f6da42167c22b14d15a
amyleblanc <amymccarthy2014@gmail.com>
2022-03-23 19:31:21 -0700

    add test for deleting interview

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -50,4 +50,28 @@ describe("Application", () => {
     const day = days.find(item => queryByText(item, "Monday"));
     expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
+
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+    const { container } = render(<Application />);
+    
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    
+    const appointment = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+    );
+    
+    fireEvent.click(getByAltText(appointment, "Delete"));
+    expect(getByText(appointment, "Confirm")).toBeInTheDocument();
+    
+    fireEvent.click(getByText(appointment, "Confirm"));
+    expect(getByText(appointment, "Deleting")).toBeInTheDocument();
+    
+    await waitForElement(() => getByAltText(appointment, "Add"));
+    
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );
+    
+    expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
+  });
 });
